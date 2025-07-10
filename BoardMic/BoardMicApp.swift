2d1e096b0b5fa3b57980f8f5aa7312864f515c29
SunnyFlops <arav.prasad@outlook.com>
2025-07-10 18:55:02 +1200

    Initial Commit

---
+++ BoardMic/BoardMicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardMicApp.swift
+//  BoardMic
+//
+//  Created by SunnyFlops on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoardMicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
